762c77afba806a5c223bfaea21331dcfbd7250ce
gvilyr <44202215+gvilyr@users.noreply.github.com>
2018-12-26 09:43:22 +0200

    Add files via upload

---
+++ ConsoleApp1/Server.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Server
+    {
+        public void listen()
+        {
+            UdpClient server = new UdpClient();
+            IPEndPoint iPEnd = new IPEndPoint(IPAddress.Any, 33173);
+            Console.WriteLine("Command and control server nnnn active");
+            EndPoint endPoint = (EndPoint)iPEnd;
+            while (true)
+            {
+                byte[] data = server.Receive(ref iPEnd);
+            }
+            
+        }
+
+    }
+}
